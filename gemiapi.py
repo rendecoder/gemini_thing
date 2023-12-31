3c5cac0862deb2aa7277b26b3d606d43a7dc6e71
rendecoder <155303999+rendecoder@users.noreply.github.com>
2023-12-31 14:12:58 +0000

    main

--- gemiapi.py
+++ gemiapi.py
@@ -1,3 +1,17 @@
-model = genai.GenerativeModel(model_name="gemini-pro-vision")
+import pathlib
+import textwrap
+import google.generativeai as genai
 
-response = model.generate_content(["What’s in this photo?", img])
+# Used to securely store your API key
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+keyshould = "o"
+print("What is your API Key? Youcan get one at Google AI Studio ")
+genai.configure(api_key=...)
